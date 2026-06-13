09518a5c08f742f26f44424a820d896667fb7e89
Draft4 <860193898@qq.com>
2026-06-13 19:30:13 +0800

    feat: enable soft wrap, configure Catppuccin integrations and highlight overrides

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- enable soft line wrap
+vim.opt.wrap = true
+
+-- spell check language
 vim.opt.spelllang = {}
 
 if vim.g.neovide then
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,8 +10,47 @@ return {
     lazy = false,
     opts = {
       flavour = "mocha", -- latte, frappe, macchiato, mocha
-      term_colors = false,
+      term_colors = true,
       transparent_background = true,
+      integrations = {
+        aerial = true,
+        diffview = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "sky",
+        },
+        noice = true,
+        telescope = {
+          enabled = true,
+        },
+        treesitter = true,
+        notify = true,
+        gitsigns = true,
+        flash = true,
+        blink_cmp = true,
+        mason = true,
+        snacks = true,
+      },
+      highlight_overrides = {
+        mocha = function(mocha)
+          return {
+            CursorLineNr = { fg = mocha.yellow },
+            TelescopeSelection = { bg = mocha.surface0 },
+            TelescopeSelectionCaret = { fg = mocha.yellow, bg = mocha.surface0 },
+            TelescopePromptPrefix = { fg = mocha.yellow },
+            FlashCurrent = { bg = mocha.peach, fg = mocha.base },
+            FlashMatch = { bg = mocha.red, fg = mocha.base },
+            FlashLabel = { bg = mocha.teal, fg = mocha.base },
+            NormalFloat = { bg = mocha.base },
+            FloatBorder = { bg = mocha.base },
+            FloatTitle = { bg = mocha.base },
+            RenderMarkdownCode = { bg = mocha.crust },
+            Pmenu = { bg = mocha.surface0 },
+            Comment = { bg = nil },
+            statusline = { bg = nil },
+          }
+        end,
+      },
     },
   },
   {
